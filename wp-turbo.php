1e715a99e6eee56db592cab0f06c289a92cf7f98
Harkály Gergő <git@harkalygergo.hu>
2022-05-20 21:59:26 +0200

    gitignore and main file added

---
+++ wp-turbo.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: WP Turbo
+Plugin URI: https://github.com/harkalygergo/wp-turbo
+Description: Universal plugin to make WordPress better, faster, safer.
+Version: 2022.05.20.
+Author: harkalygergo
+Author URI: https://www.harkalygergo.hu
+License: GPL3
+*/
