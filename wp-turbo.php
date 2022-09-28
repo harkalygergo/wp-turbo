8f553436af6a380e416a3b22434c9ebdd7431172
Harkály Gergő <git@harkalygergo.hu>
2022-09-28 13:48:56 +0200

    version update

--- wp-turbo.php
+++ wp-turbo.php
@@ -3,7 +3,7 @@
 /**
  * @wordpress-plugin
  * Plugin Name: WP Turbo
- * Version:     2022.09.27.
+ * Version:     2022.09.28.
  * Plugin URI:  https://github.com/harkalygergo/wp-turbo
  * Description: Universal plugin to make WordPress better, faster, safer.
  * Author:      Harkály Gergő
