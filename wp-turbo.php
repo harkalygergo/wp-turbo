7edc269d69bb6ecd2b0d0f93646487cf38403cbf
Harkály Gergő <git@harkalygergo.hu>
2022-11-18 19:05:34 +0100

    link manager added

--- wp-turbo.php
+++ wp-turbo.php
@@ -3,7 +3,7 @@
 /**
  * @wordpress-plugin
  * Plugin Name: WP Turbo
- * Version:     2022.11.11.
+ * Version:     2022.11.18.
  * Plugin URI:  https://github.com/harkalygergo/wp-turbo
  * Description: Universal plugin to make WordPress better, faster, safer.
  * Author:      Harkály Gergő
