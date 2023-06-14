7178088bf41a8d957481fa38cf1e9a22705b6197
Harkály Gergő <git@harkalygergo.hu>
2023-06-14 16:09:31 +0200

    SaveQueriesLogger

--- src/App/Core/Log.php
+++ src/App/Core/Log.php
@@ -14,6 +14,36 @@ class Log
     public function setHooks()
     {
         add_action( 'upgrader_process_complete', [$this, 'action_upgrader_process_complete'], 10, 2 );
+        add_action( 'shutdown', [$this, 'SaveQueriesLogger'] );
+    }
+
+    public function createWPTurboDirectory()
+    {
+        // create directory under wp-content/uploads
+        $upload_dir = wp_upload_dir();
+        $upload_dir = $upload_dir['basedir'];
+        $upload_dir = $upload_dir . '/wp-turbo';
+        if (!file_exists($upload_dir)) {
+            mkdir($upload_dir, 0777, true);
+        }
+    }
+
+    public function SaveQueriesLogger()
+    {
+        global $wpdb;
+
+        if (!is_null($wpdb->queries)) {
+            $this->createWPTurboDirectory();
+            $file = fopen(trailingslashit(WP_CONTENT_DIR) . 'uploads/wp-turbo/sqlLogs.sql', 'a');
+
+            fwrite($file, "\n\n------ NEW REQUEST [" . date("F j, Y, g:i:s a") . "] ------\n\n");
+
+            foreach ($wpdb->queries as $q) {
+                fwrite($file, $q[0] . " - ($q[1] s)" . "\n\n");
+            }
+
+            fclose($file);
+        }
     }
 
     /**
