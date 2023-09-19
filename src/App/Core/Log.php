09228a8cec5936d2c0d9c48bdd89197d01ced3db
Harkály Gergő <git@harkalygergo.hu>
2023-09-19 17:17:27 +0200

    20230919 - log visitors data to daily file

--- src/App/Core/Log.php
+++ src/App/Core/Log.php
@@ -16,8 +16,41 @@ class Log extends App
 
     public function setHooks()
     {
+        add_action( 'wp', [$this, 'saveVisitorData'] );
         add_action( 'upgrader_process_complete', [$this, 'action_upgrader_process_complete'], 10, 2 );
         add_action( 'shutdown', [$this, 'SaveQueriesLogger'] );
+    }
+
+    public function saveVisitorData(): void
+    {
+        if (is_admin()) {
+            return;
+        }
+
+        if (is_user_logged_in() && current_user_can('administrator') && current_user_can( 'manage_options' )) {
+            return;
+        }
+
+        $protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+        $currentURL = $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . $_SERVER['QUERY_STRING'];
+
+        // Get server related info
+        $user_ip_address = $_SERVER['REMOTE_ADDR'];
+        $referrer_url = $_SERVER['HTTP_REFERER'] ?? '';
+        $user_agent = $_SERVER['HTTP_USER_AGENT'];
+
+        $log = sprintf('"%s";"%s";%d;"%s";"%s";"%s"',
+            date('Y-m-d H:i:s'), // Time
+            $user_ip_address, // IP Address
+            get_current_user_id(), // User ID
+            $referrer_url, // Reffer URL
+            $currentURL, // Requested URL
+            $user_agent // User Agent
+        );
+
+        $this->stream = fopen(wp_upload_dir()['basedir']."/wp-turbo/visitorlog-".date('Ymd').".csv", "a+");
+        fwrite($this->stream, $log . "\n");
+        fclose($this->stream);
     }
 
     public function SaveQueriesLogger()
